2af0a82b7b3183c819c7390c3c0183a25be51cf1
eduard <eduard.synohub@gmail.com>
2025-12-29 00:12:43 +0100

    first steps

--- src/main/java/pl/eduard/TaskManager.java
+++ src/main/java/pl/eduard/TaskManager.java
@@ -8,11 +8,13 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class TaskManager {
+    public static final String FILENAME = "tasks.csv";
     public static void main(String[] args) {
-        addTask();
-        String[][] t = tasks("tasks.csv");
+//        addTask();
+        removeTask();
+        String[][] t = tasks(FILENAME);
         for (int i = 0; i < t.length; i++) {
-            System.out.println(Arrays.toString(t[i]));
+            System.out.println(i + ": " + Arrays.toString(t[i]));
         }
     }
 
@@ -28,9 +30,9 @@ public class TaskManager {
             case "add":
                 addTask();
                 break;
-//            case "remove":
-//                removeTask();
-//                break;
+            case "remove":
+                removeTask();
+                break;
 //            case "list":
 //                listOfTasks();
 //                break;
@@ -83,10 +85,68 @@ public class TaskManager {
         System.out.println("Is your task is important: true/false");
         addingTask[2] = scanner.nextLine();
 
-        try (FileWriter fileWriter = new FileWriter("tasks.csv", true)) {
+        try (FileWriter fileWriter = new FileWriter(FILENAME, true)) {
             fileWriter.append(addingTask[0]).append(",").append(addingTask[1]).append(",").append(addingTask[2]).append("\n");
+            System.out.println("Task successfully added!");
         } catch (IOException e) {
             System.out.println("Something went wrong.");
+        }
+    }
+
+    public static String[] readTasks() {
+        File file = new File(FILENAME);
+        String[] lines = new String[0];
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                lines = addElementToTable(lines, scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Ops... file not found.");
+        }
+        return lines;
+    }
+
+    public static String[] addElementToTable(String[] table, String elementToAdd) {
+        String[] newTable = Arrays.copyOf(table, table.length + 1);
+        newTable[newTable.length - 1] = elementToAdd;
+        return newTable;
+    }
+
+    public static void removeTask() {
+        String[][] listOfTasks = tasks(FILENAME);
+        int maxIndex = listOfTasks.length - 1;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please select number to remove:");
+
+        while (!scanner.hasNextInt()) {
+            System.out.println("Incorrect argument passed. Please enter a number:");
+            scanner.nextLine();
+        }
+
+        int index = scanner.nextInt();
+
+        while (index < 0 || index > maxIndex) {
+            System.out.println("Task with that number does not exist. Try again:");
+            index = scanner.nextInt();
+        }
+
+        String[][] newTasks = new String[maxIndex][listOfTasks[0].length];
+
+        int newIndex = 0;
+        for (int i = 0; i < listOfTasks.length; i++) {
+            if (i != index) {
+                newTasks[newIndex++] = listOfTasks[i];
+            }
+        }
+
+        try (FileWriter fileWriter = new FileWriter(FILENAME)) {
+            for (String[] task : newTasks) {
+                fileWriter.write(String.join(",", task) + "\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Cannot save data to the file.");
         }
     }
 }
